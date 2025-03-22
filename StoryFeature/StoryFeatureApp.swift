7aa9b5abf0baf41b43e923f811a504263dc3e904
Florianto <florian.tousch@hotmail.fr>
2025-03-22 17:17:37 +0400

    Initial Commit

---
+++ StoryFeature/StoryFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryFeatureApp.swift
+//  StoryFeature
+//
+//  Created by Florianto on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct StoryFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoryFeatureTests/StoryFeatureTests.swift
@@ -0,0 +1,16 @@
+//
+//  StoryFeatureTests.swift
+//  StoryFeatureTests
+//
+//  Created by Florianto on 22/03/2025.
+//
+
+import Testing
+
+struct StoryFeatureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
